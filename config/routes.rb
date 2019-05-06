50c5f13dd0c9da0d60a1d6a9a163e49361fb3f9b
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-06 12:04:20 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+ 
+resources :authors
   resources :posts
-  resources :authors
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
